de392bceb41fdb17c7c85dcd7edcc94edbe5383d
maheshprasad <mahesh223prasad@gmail.com>
2022-04-07 12:26:52 +0530

    Building List with Complex JSON Data WithOut Model Flutter Get API Call with null safety

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:apicourse/ExampleFour.dart';
 import 'package:apicourse/example_three.dart';
 import 'package:apicourse/example_two.dart';
 import 'package:apicourse/home_screen.dart';
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ExampleThree(),
+      home: ExampleFour(),
       debugShowCheckedModeBanner: false,
     );
   }
